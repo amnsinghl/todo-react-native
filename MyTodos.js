4c3bbcc3ea3b137f57dda18c7ae2b0b17ac35e47
Aman Singhal <aman.si@riva.co>
2017-12-10 12:12:38 +0530

    show all my todos

--- MyTodos.js
+++ MyTodos.js
@@ -1,19 +1,47 @@
 import React, {Component} from 'react';
 import {
     ActivityIndicator,
+    Platform,
     StyleSheet,
     Text,
     View,
     ListView,
-    Image, Button
+    Image, Button, FlatList
 } from 'react-native';
-import {StackNavigator} from "react-navigation";
-import MyTodos from "./MyTodos";
 
 const baseUrl = "https://apps.flock.co/todo/v2/";
 const token = "flockEvent=%7B%22name%22%3A%22client.pressButton%22%2C%22button%22%3A%22appLauncherButton%22%2C%22chatName%22%3A%22Aman%20Singhal%22%2C%22chat%22%3A%22u%3A10104fr1oryrf0r0%22%2C%22userName%22%3A%22Aman%20Singhal%22%2C%22locale%22%3A%22en-us%22%2C%22userId%22%3A%22u%3Aisgpwwyralhf9aso%22%7D&flockEventToken=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBJZCI6IjZkMGFhMzdiMDA5NDRlYzBhNzQyNmQzNGNhMmRmMDQ4IiwidXNlcklkIjoidTppc2dwd3d5cmFsaGY5YXNvIiwiZXhwIjoxNTEyOTY4OTA3LCJpYXQiOjE1MTIzNjQxMDcsImp0aSI6IjcwNjA5YmU2LWYzMzQtNGIzMS04Zjg1LWFjMzkwYTUyN2I2NiJ9.JCEurA8H_Uik3BN1ck4jp2gtAgYBIhP3KP-sbh4UK6I";
 
-class Roster extends Component {
+class TodoItem extends Component {
+    render() {
+        return (
+            <View>
+                <Text>{this.props.state.text}</Text>
+                <Text>{this.props.state.dueOn}</Text>
+                <Text>{this.props.state.assignedToName}</Text>
+            </View>
+        )
+    }
+}
+
+class TodoList extends Component {
+    render() {
+        return (
+            <View>
+                <Text >{this.props.state.listName}</Text>
+                <FlatList
+                    data={this.props.state.todos}
+                    renderItem={({item}) =>
+                        <TodoItem state={item}/>
+                    }
+                    keyExtractor={item => item.todoId}
+                />
+            </View>
+        );
+    }
+}
+
+export default class MyTodos extends Component {
 
     constructor() {
         super();
@@ -22,14 +50,25 @@ class Roster extends Component {
         }
     }
 
+    convertList(responseJson) {
+        let ret = [];
+        responseJson.chats.forEach((chat) => {
+            for (let li in chat.lists) {
+                let todo = chat.lists[li];
+                ret.push(todo);
+            }
+        });
+        return ret;
+    }
+
     componentDidMount() {
-        return fetch(baseUrl + 'roster?' + token)
+        return fetch(baseUrl + 'chat?' + token)
             .then((response) => response.json())
             .then((responseJson) => {
                 let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
                 this.setState({
                     isLoading: false,
-                    dataSource: ds.cloneWithRows(responseJson.sort((a, b) => a.chatName < b.chatName)),
+                    dataSource: ds.cloneWithRows(this.convertList(responseJson)),
                 }, function () {
                     // do something with new state
                 });
@@ -39,8 +78,8 @@ class Roster extends Component {
             });
     }
 
+
     render() {
-        const {navigate} = this.props.navigation;
         if (this.state.isLoading) {
             return (
                 <View style={{flex: 1, paddingTop: 20}}>
@@ -51,53 +90,13 @@ class Roster extends Component {
 
         return (
             <View style={{flex: 1, paddingTop: 20}}>
-                <Button title="My Todos" color="#00c955" onPress={() => navigate('MyTodos')}/>
                 <ListView
                     dataSource={this.state.dataSource}
                     renderRow={(rowData) =>
-                        <View style={{flex: 1, flexDirection: 'row', height: 50}}>
-                            <Image source={{uri: rowData.chatProfileImage}}
-                                   style={{width: 50, height: 50}}/>
-                            <Text style={{
-                                alignSelf: 'center',
-                                fontSize: 20,
-                                textAlign: 'center'
-                            }}>{rowData.chatName}</Text>
-                        </View>
+                        <TodoList state={rowData}/>
                     }
                 />
             </View>
         );
     }
 }
-
-const NavigationApp = StackNavigator({
-    MyTodos: {screen: MyTodos},
-    Roster: {screen: Roster}
-});
-
-export default class App extends Component {
-    render() {
-        return <NavigationApp />;
-    }
-}
-
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-});
---
+++ api.js
@@ -0,0 +1,8 @@
+// class Api {
+//     baseUrl = "https://apps.flock.co/todo/v2/";
+//     const token = "flockEvent=%7B%22name%22%3A%22client.pressButton%22%2C%22button%22%3A%22appLauncherButton%22%2C%22chatName%22%3A%22Aman%20Singhal%22%2C%22chat%22%3A%22u%3A10104fr1oryrf0r0%22%2C%22userName%22%3A%22Aman%20Singhal%22%2C%22locale%22%3A%22en-us%22%2C%22userId%22%3A%22u%3Aisgpwwyralhf9aso%22%7D&flockEventToken=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBJZCI6IjZkMGFhMzdiMDA5NDRlYzBhNzQyNmQzNGNhMmRmMDQ4IiwidXNlcklkIjoidTppc2dwd3d5cmFsaGY5YXNvIiwiZXhwIjoxNTEyOTY4OTA3LCJpYXQiOjE1MTIzNjQxMDcsImp0aSI6IjcwNjA5YmU2LWYzMzQtNGIzMS04Zjg1LWFjMzkwYTUyN2I2NiJ9.JCEurA8H_Uik3BN1ck4jp2gtAgYBIhP3KP-sbh4UK6I";
+//
+//     fetchRoster() {
+//
+//     }
+// }
