174c8099c44d008cfb1d4be9e96ae482b511bd62
Aman Singhal <aman.si@riva.co>
2017-12-10 07:44:44 +0530

    reset to vanila project

--- App.js
+++ App.js
@@ -6,17 +6,16 @@
 
 import React, { Component } from 'react';
 import {
-    ListView,
-    Platform,
-    StyleSheet,
-    Text,
-    View
+  Platform,
+  StyleSheet,
+  Text,
+  View
 } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your :P to reload,\n' +
+  android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
 
@@ -27,9 +26,12 @@ export default class App extends Component<{}> {
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <ListView
-          dataSource={}
-        />
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
       </View>
     );
   }
